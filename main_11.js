985bb98efa13ba82e9b27b1ae4769fdab762ed2d
taihua <to760621@gmail.com>
2017-03-30 11:24:28 +0800

    1.將資料全部先放到網頁上,利用name來做tabs的名稱,裡面則是內容 2.可以依照選到的tab來做切換 3.這邊的props無法更動,必須要再一層data

--- main_11.js
+++ main_11.js
@@ -1,3 +1,58 @@
+Vue.component('tabs',{
+    template:`
+    <div>
+        <div class="tabs">
+            <ul>
+                <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive}">
+                    <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+    `,
+    mounted() {
+        //console.log(this.$children);
+    },
+    data() {
+        return {tabs: []}
+    },
+    created() {
+        this.tabs = this.$children;
+    },
+    methods: {
+        selectTab(selectedTab){
+            this.tabs.forEach(tab => {
+                tab.isActive = tab.name === selectedTab.name;
+            });
+        }
+    }
+});
+
+Vue.component('tab',{
+    template:`
+        <div>
+            <slot></slot>
+        </div>
+    `,
+    props:{
+        name: {require:true},
+        selected: {default: false}
+    },
+    data() {
+        return {isActive: false};
+    },
+    mounted() {
+        this.isActive = this.selected;
+    },
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    }
+});
 
 new Vue({
     el:'#root'
