deec35731ac8e061aeff7a5a7688386d445a7840
taihua <to760621@gmail.com>
2017-03-31 11:05:13 +0800

    init 13 原本的event是將function寫在Vue裡面  再帶到裡面的component去觸發 改成是用on去寫event ,  裡面還是用$emit去觸發

---
+++ main_13.js
@@ -0,0 +1,20 @@
+window.Event = new Vue();
+
+Vue.component('coupon',{
+    template:`<input placeholder="Enter your coupon code" @blur="onCouponApplied"/>`,
+    methods:{
+        onCouponApplied() {
+            Event.$emit('applied');
+        }
+    }
+});
+
+new Vue({
+    el:'#root',
+    data: {
+        couponApplied: false
+    },
+    created() {
+        Event.$on('applied',() => alert('Handing it'));
+    }
+});
