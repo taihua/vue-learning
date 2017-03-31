83ccc1f74430a99e48a79091a95e261aea445984
taihua <to760621@gmail.com>
2017-03-31 10:46:26 +0800

    從初始化組件提供methods 跟 event 讓底下的 component來執行

--- main_12.js
+++ main_12.js
@@ -1,70 +1,20 @@
-Vue.component('tabs',{
-    template:`
-    <div>
-        <div class="tabs">
-            <ul>
-                <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive}">
-                    <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
-                </li>
-            </ul>
-        </div>
-        <div class="tabs-details">
-            <slot></slot>
-        </div>
-    </div>
-    `,
-    mounted() {
-        //console.log(this.$children);
-    },
-    data() {
-        return {tabs: []}
-    },
-    created() {
-        this.tabs = this.$children;
-    },
-    methods: {
-        selectTab(selectedTab){
-            this.tabs.forEach(tab => {
-                tab.isActive = tab.name === selectedTab.name;
-            });
-        }
-    }
-});
-
-Vue.component('tab',{
-    template:`
-        <div v-show="isActive">
-            <slot></slot>
-        </div>
-    `,
-    props:{
-        name: {require:true},
-        selected: {
-            default:false
-        }
-    },
-    data() {
-        return {isActive: false};
-    },
-    mounted() {
-        let hash_tag = location.href.split('#').pop();
-        //return this.name.toLowerCase().replace(/ /g, '-') === hash_tag;
-        if (hash_tag)
-        {
-            this.isActive = this.name.toLowerCase().replace(/ /g, '-') === hash_tag;
-        }
-        else
-        {
-            this.isActive = this.selected;
-        }
-    },
-    computed: {
-        href() {
-            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+Vue.component('coupon',{
+    template:`<input placeholder="Enter your coupon code" @blur="onCouponApplied"/>`,
+    methods:{
+        onCouponApplied() {
+            this.$emit('applied');
         }
     }
 });
 
 new Vue({
-    el:'#root'
+    el:'#root',
+    data: {
+        couponApplied: false
+    },
+    methods:{
+        onCouponApplied() {
+            this.couponApplied = true;
+        }
+    }
 });
