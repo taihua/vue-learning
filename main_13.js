d92cb87eed3070b651ccd2e79b936fc984fa7d89
taihua <to760621@gmail.com>
2017-03-31 11:08:07 +0800

    宣告全域的物件 將$emit , $on 換成是自定義的function

--- main_13.js
+++ main_13.js
@@ -1,10 +1,24 @@
-window.Event = new Vue();
+window.Event = new class{
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+}
+
+
 
 Vue.component('coupon',{
     template:`<input placeholder="Enter your coupon code" @blur="onCouponApplied"/>`,
     methods:{
         onCouponApplied() {
-            Event.$emit('applied');
+            Event.fire('applied');
         }
     }
 });
@@ -15,6 +29,6 @@ new Vue({
         couponApplied: false
     },
     created() {
-        Event.$on('applied',() => alert('Handing it'));
+        Event.listen('applied',() => alert('Handing it'));
     }
 });
